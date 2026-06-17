3c353e6848741d97e9ec68f81193fb18ca5eaf9f
Pablo <pablo.jahnenmelladogmail.com>
2026-06-17 17:21:43 +0200

    fix: use uvicorn for production in Docker

--- fastapi_backend/start.sh
+++ fastapi_backend/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-export PATH="$PATH:/c/Users/Verwenden/.local/bin"
 
 if [ -f /.dockerenv ]; then
     echo "Running in Docker"
-    fastapi dev app/main.py --host 0.0.0.0 --port 8000 --reload &
-    python watcher.py
+    exec uvicorn app.main:app --host 0.0.0.0 --port 8000
 else
     echo "Running locally with uv"
     uv run fastapi dev app/main.py --host 0.0.0.0 --port 8000 --reload &
     uv run python watcher.py
+    wait
 fi
-
-wait
-
-
